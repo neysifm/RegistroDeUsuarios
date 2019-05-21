6db428bc00d3a052b907a705c10f427f5a375e18
Neysi Alexandra Fermin Minaya <neysi_fermin@ucne.edu.do>
2019-05-21 11:35:56 -0400

    Agregadndo Clase Cargos y Modificando Contexto

---
+++ RegistroDeUsuarios/Entidades/Cargos.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+
+namespace RegistroDeUsuarios.Entidades
+{
+    public class Cargos
+    {
+        [Key]
+
+        public int CargosId       { get; set; }
+        public String Descripcion { get; set; }
+
+        public Cargos()
+        {
+            CargosId = 0;
+            Descripcion = String.Empty;
+        }
+    }
+}
